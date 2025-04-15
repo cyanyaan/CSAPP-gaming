21934358400440f845c62d2df9c5447a74ff1432
cyanyaan <ankitsarkar431@proton.me>
2025-04-15 22:24:49 +0530

    gg

--- representing and manipulating information/byterep.c
+++ representing and manipulating information/byterep.c
@@ -6,7 +6,7 @@ void show_bytes(byte_pointer start, size_t len)
 {
     for (int i = 0; i < len; i++)
     {
-        printf("%.2x", start[i]);
+        printf(" %.2x", start[i]);
     }
     printf("\n");
 }
@@ -21,4 +21,17 @@ void show_float(float x) {
 
 void show_pointer(void *x) {
     show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+void test_show_bytes(int val) {
+    int ival = val;
+    float fval = (float) ival;
+    int *pval = &ival;
+    show_int(ival);
+    show_float(fval);
+    show_pointer(pval);
+    }
+
+int main(){
+    test_show_bytes(12345);
 }
