54818af1d744350f63896d739ca9114553d48ae1
cyanyaan <ankitsarkar431@gmail.com>
2025-04-25 21:34:19 +0530

    lets go brandon

--- representing and manipulating information/problems/2.13/main.c
+++ representing and manipulating information/problems/2.13/main.c
@@ -1,14 +1,17 @@
-
 /* Declarations of functions implementing operations bis and bic */
 int bis(int x, int m);
 int bic(int x, int m);
+
 /* Compute x|y using only calls to functions bis and bic */
 int bool_or(int x, int y) {
-int result =bis(x,y);
+int result = bis(x, y);
 return result;
 }
+
 /* Compute x^y using only calls to functions bis and bic */
 int bool_xor(int x, int y) {
-int result = bis(bic(x,y), bic(y, x));
+int a = bic(x, y);
+int b = bic(y, x);
+int result = bis(a, b);
 return result;
 }
